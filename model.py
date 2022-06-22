571d02df31364efa91addad59c4909af161fa3b4
Michael Lobman <michael.lobman@gmail.com>
2022-06-22 17:33:48 -0400

    Encode sonnet text as integers

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from ntpath import join
 import tensorflow as tf
 import numpy as np
 import re
@@ -26,50 +25,32 @@ for sonnet in sonnets:
 # Remove sonnets from data that do not conform to standard 14-line pattern.
 # Generate vocab
 
-vocab = set()
 for sonnet in split_sonnets:
     if len(sonnet) != 14:
         split_sonnets.remove(sonnet)
-    else:
-        for line in sonnet:
-            for c in line:
-                vocab.add(c)
 
 # Join individual sonnets on a newline character
 
 joined_sonnets = list()
-
 for sonnet in split_sonnets:
     joined_sonnets.append('\n'.join(sonnet))
-print(joined_sonnets[10])
 
-# Convert sonnets to tensor (shape=152, 14)
+# Generate vocab set
 
-# might not want this as a tensor
+vocab = set()
+for sonnet in joined_sonnets:
+    for c in sonnet:
+        vocab.add(c)
 
-# sonnets_tensor = tf.convert_to_tensor(split_sonnets)
-# flattened_tensor = tf.reshape(split_sonnets, [-1])
+# Encode sonnets.
 
-# MAX LINE IS 59 characters. Will need to pad lines to 60 characters?
-
-# Encode text.
 char2idx = {u:i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
 
-# Below does not work. Need a way to encode the text.
-# May have to allow the line breaks? 
-# can join the split sonnets on them! yes, join with a '\n'
+def text_to_int(text):
+    return np.array([char2idx[c] for c in text])
 
-# def text_to_int(sonnet_list):
-#     for sonnet in sonnet_list:
-#         for line in sonnet:
-#             return np.array([char2idx[c] for c in line])
-
-# text_as_int = text_to_int(split_sonnets)
-
-# print(text_as_int)
-# # Create function to decode text.
-
-
+encoded_sonnets = list(map(text_to_int, joined_sonnets))
 
 # divide data into train and test groups
+
