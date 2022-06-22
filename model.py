751fbc0b7b2065557fa4c448bb59e1430b41a33f
Michael Lobman <michael.lobman@gmail.com>
2022-06-22 16:28:21 -0400

    Generate vocab set from sonnet text

--- model.py
+++ model.py
@@ -23,16 +23,22 @@ for sonnet in sonnets:
         split_sonnets.append(updated_sonnet)
 
 # Remove sonnets from data that do not conform to standard 14-line pattern.
+# Generate vocab
 
+vocab = set()
 for sonnet in split_sonnets:
     if len(sonnet) != 14:
         split_sonnets.remove(sonnet)
+    else:
+        for line in sonnet:
+            for c in line:
+                vocab.add(c)
 
 # Convert sonnets to tensor (shape=152, 14)
 
 sonnets_tensor = tf.convert_to_tensor(split_sonnets)
 
-# MAX LINE IS 59 characters. Will need to pad lines to 60 characters.
+# MAX LINE IS 59 characters. Will need to pad lines to 60 characters?
 
 # create vocab set.
 
