ba0aba311a39aeefcdfad56162c18544d5fb6ef3
Michael Lobman <michael.lobman@gmail.com>
2022-06-22 12:59:57 -0400

    Format training and testing data, convert to tensor

--- model.py
+++ model.py
@@ -2,31 +2,35 @@ import tensorflow as tf
 import numpy as np
 import re
 
+# Open plain text file, split the file into "Sonnets" on their delineating number.
+
 text = open('./downloads/sonnets.txt').read()
 sonnets = re.split('[0-9]', text)
 sonnets = [sonnet.strip() for sonnet in sonnets[1:]]
 
-sonnets_tensor = tf.convert_to_tensor(sonnets)
+# Split each sonnet into a 1D list of its respective lines. Strip whitespace, remove 
 
-# i think I need to use a map function here
-# first, see the shape of the tensor, I think it's one long stream...
-# first may want to divide it into each sonnet, flat tensor of shape 1 with 154 sonnets
-# next, apply  amapy to each tensor, or mess with its shape, reshape it to 154 tensors of shape 15
+split_sonnets = list()
 
+for sonnet in sonnets:
+    split = sonnet.split('\n')
+    updated_sonnet = list()
+    for line in split:
+        stripped = line.strip()
+        if stripped:
+            updated_sonnet.append(line.strip())
+    if updated_sonnet:
+        split_sonnets.append(updated_sonnet)
 
-# Below code makes each sonnet a list
+# Remove sonnets from data that do not conform to standard 14-line pattern.
 
-# sonnet_list = list()
+for sonnet in split_sonnets:
+    if len(sonnet) != 14:
+        split_sonnets.remove(sonnet)
 
-# for sonnet in sonnets:
-#     split = sonnet.split('\n')
-#     updated_sonnet = list()
-#     for line in split:
-#         stripped = line.strip()
-#         if stripped:
-#             updated_sonnet.append(line.strip())
-#     if updated_sonnet:
-#         sonnet_list.append(updated_sonnet)
+# Convert sonnets to tensor (shape=152, 14)
+
+sonnets_tensor = tf.convert_to_tensor(split_sonnets)
 
 # MAX LINE IS 59 characters. Will need to pad lines to 60 characters.
 
